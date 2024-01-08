ef475a89062a6c3e18e75e90a2e1b51a3500a523
David <dmaine6711@gmail.com>
2024-01-08 14:17:24 -0500

    finish phase 4

--- aa15-react-class-components-refactor/src/components/Clock.jsx
+++ aa15-react-class-components-refactor/src/components/Clock.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export class ClockToggle extends React.Component {
   render () {
@@ -14,64 +14,112 @@ export class ClockToggle extends React.Component {
   }
 } 
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: new Date(),
+// class Clock extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       time: new Date(),
+//     };
+//   }
+  
+//   componentDidMount() {
+//     this.interval = setInterval(this.tick, 1000);
+//   }
+  
+//   componentWillUnmount() {
+//     console.log("Clearing Clock interval!")
+//     clearInterval(this.interval);
+//   }
+  
+//   tick = () => {
+//     this.setState({ time: new Date() });
+//   }
+
+//   render() {
+//     let hours = this.state.time.getHours();
+//     let minutes = this.state.time.getMinutes();
+//     let seconds = this.state.time.getSeconds();
+//     hours = (hours < 10) ? `0${hours}` : hours;
+//     minutes = (minutes < 10) ? `0${minutes}` : minutes;
+//     seconds = (seconds < 10) ? `0${seconds}` : seconds;
+
+//     const timezone = this.state.time
+//       .toTimeString() // Form: "14:39:07 GMT-0600 (PDT)"
+//       .replace(/[^A-Z]/g, "") // Strip out all but capitals
+//       .slice(3); // Eliminate initial GMT
+
+//     return (
+//       <section className="clock-section">
+//         <h1>Clock</h1>
+//         <div className='clock'>
+//           <p className="time">
+//             <span>
+//               Time:
+//             </span>
+//             <span>
+//               {`${hours}:${minutes}:${seconds} ${timezone}`}
+//             </span>
+//           </p>
+//           <p className="date">
+//             <span>
+//               Date: 
+//             </span>
+//             <span>
+//               {this.state.time.toDateString()}
+//             </span>
+//           </p>
+//         </div>
+//       </section>
+//     );
+//   }
+// }
+
+
+const Clock = (props) => {
+  const [time, setTime] = useState(new Date());
+  let interval;
+
+  useEffect(() => {
+    interval = setInterval(tick, 1000);
+    return () => {
+      clearInterval(interval);
     };
-  }
-  
-  componentDidMount() {
-    this.interval = setInterval(this.tick, 1000);
-  }
-  
-  componentWillUnmount() {
-    console.log("Clearing Clock interval!")
-    clearInterval(this.interval);
-  }
-  
-  tick = () => {
-    this.setState({ time: new Date() });
-  }
+  });
 
-  render() {
-    let hours = this.state.time.getHours();
-    let minutes = this.state.time.getMinutes();
-    let seconds = this.state.time.getSeconds();
-    hours = (hours < 10) ? `0${hours}` : hours;
-    minutes = (minutes < 10) ? `0${minutes}` : minutes;
-    seconds = (seconds < 10) ? `0${seconds}` : seconds;
+  const tick = () => {
+    setTime(new Date());
+  };
 
-    const timezone = this.state.time
-      .toTimeString() // Form: "14:39:07 GMT-0600 (PDT)"
-      .replace(/[^A-Z]/g, "") // Strip out all but capitals
-      .slice(3); // Eliminate initial GMT
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
+  hours = hours < 10 ? `0${hours}` : hours;
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
 
-    return (
-      <section className="clock-section">
-        <h1>Clock</h1>
-        <div className='clock'>
-          <p className="time">
-            <span>
-              Time:
-            </span>
-            <span>
-              {`${hours}:${minutes}:${seconds} ${timezone}`}
-            </span>
-          </p>
-          <p className="date">
-            <span>
-              Date: 
-            </span>
-            <span>
-              {this.state.time.toDateString()}
-            </span>
-          </p>
-        </div>
-      </section>
-    );
-  }
+  const timezone = time
+    .toTimeString() // Form: "14:39:07 GMT-0600 (PDT)"
+    .replace(/[^A-Z]/g, "") // Strip out all but capitals
+    .slice(3); // Eliminate initial GMT
+
+  return (
+    <section className="clock-section">
+      <h1>Clock</h1>
+      <div className="clock">
+        <p className="time">
+          <span>Time:</span>
+          <span>{`${hours}:${minutes}:${seconds} ${timezone}`}</span>
+        </p>
+        <p className="date">
+          <span>Date:</span>
+          <span>{time.toDateString()}</span>
+        </p>
+      </div>
+    </section>
+  );
+
+
 }
+
 
 export default Clock;
